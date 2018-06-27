13da87cd34609182a3e094865506164a577cad1c
Andrea <s9ankupi@stud.uni-saarland.de>
2018-06-27 12:59:32 +0200

    mean likelihood

--- simulation.py
+++ simulation.py
@@ -1,7 +1,9 @@
 import math
 import random
+import scipy.stats
 
-from scipy.optimize import minimize
+import numpy as np
+from scipy.optimize import minimize, basinhopping
 from distribution import createDistri
 
 
@@ -13,6 +15,7 @@ class Simulation:
     lowerStrand = []
     # if DNMT true, no knock out
     L = 0 #number of CpGs
+    distances = []
     #               rho             tau             mhy          delta
     #           (disassociation   (association  (maintenance   (de novo methyl
     #               prob)           prob)        methyl prob)       prob)
@@ -20,14 +23,13 @@ class Simulation:
                      [0.5,          0.5,          0,              1],   #DNMT3d (daughter strand)
                      [0.5,          0.5,          0,                1]]     #DNMT3p (parent strand)
 
-    def __init__(self, filename, DNMT1=True, DNMT3=True):
+    def __init__(self, filename, distances, DNMT1=True, DNMT3=True):
         distribution, self.L, numOfPatterns = createDistri(filename) #distribution of methylation patterns
         self.create_initial_distr(distribution)
         dist = [i*numOfPatterns for i in distribution]
+        self.distances = distances
 
         self.minimizeLH(self.probabilities[0], dist, False)
-        self.minimizeLH(self.probabilities[1], dist, True)
-
     def create_initial_distr(self, distribution):
         """
         choose an initial distribution
@@ -85,7 +87,7 @@ class Simulation:
                 upperStrand, lowerStrand = self.simulateDNMT(allProbs[1], upperStrand, lowerStrand)
                 lowerStrand, upperStrand = self.simulateDNMT(allProbs[2], lowerStrand, upperStrand)
             upperStrand = upperStrand if random.random() <= 0.5 else lowerStrand
-
+        #print(upperStrand, lowerStrand)
         return upperStrand, lowerStrand
 
     def simulateDNMT(self, probs, parentS, daughterS):
@@ -100,25 +102,33 @@ class Simulation:
         tau = probs[1]
         mhy = probs[2]
         delta = probs[3]
-        bound = True if random.random() <= 1-rho else False
-        for pos in range(self.L):
-            #if enzyme stays associated
-            if bound:
-                #probabilty for de novo or maintenance methylation
-                if (parentS[pos] and random.random() <= mhy) or (not parentS[pos] and random.random() <= delta):
-                    daughterS[pos] = 1
-                bound = True if random.random() <= 1-rho else False
+        bound = False
+        CpG = True #if current position is a CpG
+        length = sum(self.distances) + len(self.distances) + 1  # total number of positions
+        cpgPos = 0 #cpg positions
+        cpg = -1 #counting current CpG
+        for pos in range(length):
+            #if current position is a CpG
+            if pos == cpgPos:
+                cpg += 1
+                cpgPos = cpgPos + self.distances[cpg] + 1 if cpg <= self.L-2 else 0 #compute next CpG
+                CpG = True
             else:
-                bound = True if random.random() <= tau else False
+                CpG = False
+            #if enzyme stays associated
+            if (not bound and random.random() <= tau) or bound:
+                #probabilty for de novo or maintenance methylation
+                if CpG and ((parentS[cpg] and random.random() <= mhy) or (not parentS[cpg] and random.random() <= delta)):
+                   daughterS[cpg] = 1
+                bound = True if random.random() <= 1-rho else False
         return parentS, daughterS
 
-    def computeLH(self, probabilities, distribution, DNMT1KO, iterations=10000):
+    def computeLH(self, probabilities, distribution, DNMT1KO):
         '''
         perform multiple iterations of simulation and compute likelihood
         :param probabilities: parameters for simulation
         :param distribution: original pattern distribution
         :param DNMT1KO: True if DNMT1KO, False if DNMT3KO
-        :param iterations: number of iterations of simulation
         :return: negative likelihood
         '''
         allProbs = [] * 3
@@ -129,24 +139,41 @@ class Simulation:
             allProbs.append(probabilities)
         else:
            allProbs.append(probabilities)
-        patterns = dict()
-        likelihood = 0
+        lhs = []
+        error = 1
+        threshold = 0
 
-        #perform multiple iterations and store resulting patterns
-        for i in range(iterations):
-            upperStrand, lowerStrand = self.simulate(allProbs, DNMT1=not DNMT1KO, DNMT3=DNMT1KO)
-            pattern = 0
-            for l in range(self.L):
-                pattern += 4 ** (self.L - l - 1) * (upperStrand[l] + lowerStrand[l]*2)
-            patterns[pattern] = patterns.get(pattern, 0) + 1
-        patterns = {k: float(v/iterations) for k, v in patterns.items()}
+        while error > threshold:
+            for i in range(10):
+                patterns = dict()
+                likelihood = 1.0
+                #perform multiple iterations and store resulting patterns
+                for i in range(10000):
+                    upperStrand, lowerStrand = self.simulate(allProbs, DNMT1=not DNMT1KO, DNMT3=DNMT1KO)
+                    pattern = 0
+                    for l in range(self.L):
+                        pattern += 4 ** (self.L - l - 1) * (upperStrand[l] + lowerStrand[l]*2)
+                    patterns[pattern] = patterns.get(pattern, 0) + 1
+                patterns = {k: float(v/10000) for k, v in patterns.items()}
 
-        #compute likelihood
-        for k, v in patterns.items():
-        #if v is 1.0 here, likelihood is 0?!
-            likelihood += distribution[k] * math.log(v)
-        #print(-likelihood, probabilities)
-        return -likelihood #minimize negative log-Likelihood <=> maximize (log)Likelihood
+                #compute likelihood
+                epsilon = 0.000001  # add epsilon to all distribution values which are 0
+                for k, v in enumerate(distribution):
+                    simDistri = patterns[k] if k in patterns else epsilon
+                    likelihood += v * math.log(simDistri)
+                print(likelihood, probabilities)
+                lhs.append(-likelihood)
+            mean, error = self.mean_confidence_interval(lhs)
+            threshold = mean*0.005
+            print(error, threshold)
+        return -mean #minimize negative log-Likelihood <=> maximize (log)Likelihood
+
+    def mean_confidence_interval(self, data, confidence=0.95):
+        a = 1.0*np.array(data)
+        n = len(a)
+        m, se = np.mean(data), scipy.stats.sem(a)
+        h = se * scipy.stats.t._ppf((1+confidence)/2., n-1)
+        return m, h
 
     def minimizeLH(self, probabilities, distribution, DNMT1KO):
         '''
@@ -159,10 +186,14 @@ class Simulation:
         args = (distribution, DNMT1KO)
         #bounds for parameter space
         bnds = ((0, 1), (0, 1), (0, 1), (0, 1))  # here 4 parameters bound between 0 and 1
-        sol = minimize(self.computeLH, probabilities, args=args, bounds=bnds, options={'disp': True})
+        #sol = minimize(self.computeLH, probabilities, args=args, bounds=bnds, options={'disp': True})
+        #sol = minimize(self.computeLH, probabilities, method='Nelder-Mead', args=args, bounds=bnds, options={'disp': True})
         #sol = minimize(self.computeLH, probabilities, method='L-BFGS-B', args=args, bounds=bnds, options={'disp': True})
-        #sol = minimize(self.computeLH, probabilities, method='SLSQP', args=args, bounds=bnds, options={'disp': True})
+        # use method L-BFGS-B because the problem is smooth and bounded
+        minimizer_kwargs = dict(method="L-BFGS-B", bounds=bnds, args=args)
+        sol = basinhopping(self.computeLH, probabilities, minimizer_kwargs=minimizer_kwargs)
         print(sol)
 
 
-Simulation("Daten/IAPDnmt1KO.txt", DNMT1=False)
+#do we consider the bps in front of first CpG?
+Simulation("Daten/IAPDnmt1KO.txt", [2, 6, 3, 35, 7], True, False)
