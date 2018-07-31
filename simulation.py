b1c5febf43e6e6ee89bee547288d06c5713d0597
Andrea <s9ankupi@stud.uni-saarland.de>
2018-07-31 16:11:06 +0200

    started ABC

--- simulation.py
+++ simulation.py
@@ -5,7 +5,18 @@ import scipy.stats
 import numpy as np
 from scipy.optimize import minimize, basinhopping
 from distribution import createDistri
+from abcpmc import Sampler, ConstEps
+#from matplotlib import plot
 
+class Prior(object):
+    def __init__(self):
+        self.probs = []
+
+    def __call__(self, theta=None):
+        self.probs = []
+        for i in range(5):
+            self.probs.append(random.random())
+        return self.probs
 
 class Simulation:
     """
@@ -15,7 +26,9 @@ class Simulation:
     lowerStrand = []
     # if DNMT true, no knock out
     L = 0 #number of CpGs
-    distances = []
+    distances = [] #distances between CpGs
+    distributionKO = dict() #pattern distribution in KO-file
+    DNMT1KO = True
     #               rho             tau             mhy          delta
     #           (disassociation   (association  (maintenance   (de novo methyl
     #               prob)           prob)        methyl prob)       prob)
@@ -28,11 +41,10 @@ class Simulation:
         distributionKO, self.L, numOfPatterns = createDistri(KOfile) # distribution of methylation patterns after DNMT KO
         self.create_initial_distr(distributionWT) # create initial distribution from WT
         # number of occurrences of each pattern in DNMT KO data for comparison with outcome of simulation
-        dist = [i*numOfPatterns for i in distributionKO]
+        self.distributionKO = [i*numOfPatterns for i in distributionKO]
         self.distances = distances # distances between CpGs
+        self.DNMT1KO = DNMT1KO
 
-        self.minimizeLH(self.probabilities[0], dist, DNMT1KO)
-        #self.minimizeLH([0,0,0.5,0.5], dist, DNMT1KO)
     def create_initial_distr(self, distribution):
         """
         choose an initial distribution
@@ -126,16 +138,14 @@ class Simulation:
                 bound = True if random.random() <= 1-rho else False
         return parentS, daughterS
 
-    def computeLH(self, probabilities, distribution, DNMT1KO):
+    def computePatternDistribution(self, probabilities):
         '''
-        perform multiple iterations of simulation and compute likelihood
+        perform multiple iterations of simulation and compute pattern distribution of simulation
         :param probabilities: parameters for simulation
-        :param distribution: original pattern distribution
-        :param DNMT1KO: True if DNMT1KO, False if DNMT3KO
-        :return: negative likelihood
+        :return: pattern distribution of simulation
         '''
         allProbs = [] * 3
-        if DNMT1KO:
+        if self.DNMT1KO:
             #prob DNMT3p = DNMT3d???
             allProbs.append([])
             allProbs.append(probabilities)
@@ -149,20 +159,28 @@ class Simulation:
         #while error > threshold:
             #for i in range(10):
         patterns = dict()
-        #likelihood = 1.0
-        likelihood = 0.0 #for log-likelihood
         #perform multiple iterations and store resulting patterns
         for i in range(10000):
-            upperStrand, lowerStrand = self.simulate(allProbs, DNMT1=not DNMT1KO, DNMT3=DNMT1KO)
+            upperStrand, lowerStrand = self.simulate(allProbs, DNMT1=not self.DNMT1KO, DNMT3=self.DNMT1KO)
             pattern = 0
             for l in range(self.L):
                 pattern += 4 ** (self.L - l - 1) * (upperStrand[l] + lowerStrand[l]*2)
             patterns[pattern] = patterns.get(pattern, 0) + 1
         patterns = {k: float(v/10000) for k, v in patterns.items()}
+        return patterns
 
+    def computeLH(self, probabilities):
+        '''
+        compute likelihood
+        :param probabilities: parameters for simulation
+        :return: negative likelihood
+        '''
         #compute likelihood
+        #likelihood = 1.0
+        likelihood = 0.0 #for log-likelihood
         epsilon = 0.0000001  # add epsilon to all distribution values which are 0
-        for k, v in enumerate(distribution):
+        patterns = self.computePatternDistribution(probabilities)
+        for k, v in enumerate(self.distributionKO):
             if v != 0:
                 simDistri = patterns[k] if k in patterns else epsilon
                 likelihood += simDistri * math.log(v) #for log-likelihood
@@ -176,31 +194,63 @@ class Simulation:
         return -likelihood
 
     def mean_confidence_interval(self, data, confidence=0.95):
+        '''
+        computes confidence interval for given data and confidence level
+        :param data:
+        :param confidence: confidence level
+        :return: mean, confidence interval
+        '''
         a = 1.0*np.array(data)
         n = len(a)
         m, se = np.mean(data), scipy.stats.sem(a)
         h = se * scipy.stats.t._ppf((1+confidence)/2., n-1)
         return m, h
 
-    def minimizeLH(self, probabilities, distribution, DNMT1KO):
+    def distanceFunction(self, patterns):
+        '''
+        A distance function for an ABC-algorithm, computes the distance of two distributions as sum of differences
+        between all counts of patterns
+        :param patterns: pattern distribution of simulation
+        :return: distance between pattern distribution of simulation and pattern distribution of KO-file
+        '''
+        dist = 0
+        for k,v in self.distributionKO.items():
+            dist += abs(patterns[k]-v)
+        return dist
+
+    def minimizeLH(self, probabilities):
         '''
         use parameter optimization to minimize likelihood
         :param probabilities: initial parameters
-        :param distribution: original pattern distribution
-        :param DNMT1KO: True if DNMT1KO, False if DNMT3KO
         '''
         # extra arguments passed to the objective function and its derivatives
-        args = (distribution, DNMT1KO)
         # bounds for parameter space
         bnds = ((0, 1), (0, 1), (0, 1), (0, 1))  # here 4 parameters bound between 0 and 1
-        #sol = minimize(self.computeLH, probabilities, args=args, bounds=bnds, options={'disp': True})
-        sol = minimize(self.computeLH, probabilities, method='L-BFGS-B', args=args, bounds=bnds, options={'disp': True})
+        #sol = minimize(self.computeLH, probabilities, bounds=bnds, options={'disp': True})
         # use method L-BFGS-B because the problem is smooth and bounded
-        #minimizer_kwargs = dict(method="L-BFGS-B", bounds=bnds, args=args, options={'disp': True})
+        sol = minimize(self.computeLH, probabilities, method='L-BFGS-B', bounds=bnds, options={'disp': True})
+        #minimizer_kwargs = dict(method="L-BFGS-B", bounds=bnds, options={'disp': True})
         #sol = basinhopping(self.computeLH, probabilities, minimizer_kwargs=minimizer_kwargs)
         print(sol)
 
+    def ABC(self, probabilities):
+        '''
+        approximate bayesian computation
+        :param probabilities: initial parameters
+        :return:
+        '''
+        patterns = self.computePatternDistribution(probabilities)
+        sampler = Sampler(1000, patterns, self.computePatternDistribution, self.distanceFunction)
+        eps = ConstEps(100, len(patterns))
+        pools = sampler.sample(Prior(), eps)
+        thetas = np.array([pool.thetas for pool in pools])
+        #plot(thetas)
 
-Simulation("Daten/ySatWTJ1C.txt", "Daten/ySatDNMT1KO.txt", [13, 14])
-#Simulation("Daten/ySatWTJ1C.txt", "Daten/IAPDNMT1KO.txt", [2, 6, 3, 35, 7])
-#Simulation("Daten/ySatWTJ1C.txt", "Daten/ySatDNMT3abKO.txt", [13, 14], False)
+
+#DNMT1KO:
+sim = Simulation("Daten/ySatWTJ1C.txt", "Daten/ySatDNMT1KO.txt", [13, 14])
+#sim.minimizeLH(sim.probabilities[0])
+#DNMT3KO:
+#sim = Simulation("Daten/ySatWTJ1C.txt", "Daten/ySatDNMT3abKO.txt", [13, 14], False)
+#sim.minimizeLH(sim.probabilities[1])
+sim.minimizeLH([0,0.5,0.5,0.5])
